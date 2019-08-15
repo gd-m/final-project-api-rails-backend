b24dca14dd4aa8d5e4ff7914f09b6b68894cb230
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-08-14 22:40:27 -0500

    namespacing routes and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+git = User.create(username: "git69", first_name: "Gitanjali", last_name: "Mule", password: "password")
