d6e074b4b860a441a212110c14283279e5fc6c84
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-08-14 23:56:22 -0500

    added migration

--- db/migrate/20190815034220_create_notes.rb
+++ db/migrate/20190815034220_create_notes.rb
@@ -2,8 +2,8 @@ class CreateNotes < ActiveRecord::Migration[5.2]
   def change
     create_table :notes do |t|
       t.string :content
-      t.user :belongs_to
-      t.book :belongs_to
+      t.integer :user_id
+      t.integer :book_id
 
       t.timestamps
     end
