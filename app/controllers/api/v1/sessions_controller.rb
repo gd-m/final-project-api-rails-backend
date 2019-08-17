52cff36ef083147c93c8e52212751f6e0beb81b8
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-08-17 18:02:03 -0500

    added get user and current user method to set sessions

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+        @user = User.find_by(username: params[:session][:username])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            user_json = UserSerializer.new(@user).serialized_json
+            render json: user_json
+        else 
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            user_json = UserSerializer.new(current_user).serialized_json
+            render json: user_json
+        else
+            render json: {
+                error: "No on Logged In"
+            }
+        end
+    end
+end
